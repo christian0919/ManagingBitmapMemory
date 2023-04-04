aa323ef40df12662a14e836dddf268fe14da6f81
Christian del Angel <christian.lopez190900@gmail.com>
2023-04-03 19:58:29 -0600

    The components were placed in the Content pane

--- ManagingBitmapMemory/src/bitmapmemorymanagement/Main.java
+++ ManagingBitmapMemory/src/bitmapmemorymanagement/Main.java
@@ -11,39 +11,11 @@ public class Main {
 	
 	public static void main(String[] args) {	
 	//Launch view
-		/**
+
 		MainView frame = new MainView();
 		frame.setVisible(true);
-		**/
-		
-	//check bitmap class
-		BitMap mapabits= new BitMap();
-		// bitPosition, positionArray
-		mapabits.SetBit(0, 0);
-		mapabits.SetBit(1, 0);
-		mapabits.SetBit(2, 0);
-		mapabits.SetBit(3, 0);
-		mapabits.SetBit(4, 0);
-		mapabits.SetBit(5, 0);
-		mapabits.SetBit(6, 0);
-		mapabits.SetBit(7, 0);
-		mapabits.SetBit(0, 1);
-		mapabits.SetBit(1, 1);
-		mapabits.SetBit(2, 1);
-		mapabits.SetBit(3, 1);
-		mapabits.SetBit(4, 1);
-		mapabits.SetBit(5, 1);
-		mapabits.SetBit(6, 1);
-		mapabits.SetBit(7, 1);
-		mapabits.PrintMap();
-		int[] ans=mapabits.LookForHole(3);
-		System.out.println(ans[0]+"|"+ans[1]);
-		mapabits.AsingProcess(200);
-		mapabits.PrintMap();
-		ans=mapabits.LookForHole(500);
-		System.out.println(ans[0]+"|"+ans[1]);
 
-		System.out.println(mapabits.LookForArrayPosition(66));
+
 	}
 
 }
--- ManagingBitmapMemory/src/gui/MainView.java
+++ ManagingBitmapMemory/src/gui/MainView.java
@@ -5,10 +5,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JSlider;
+import java.awt.Button;
+import javax.swing.JList;
+import javax.swing.JEditorPane;
+import javax.swing.JComboBox;
+import java.awt.Canvas;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import java.awt.Label;
+import javax.swing.JLabel;
 
 public class MainView extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField NameProcessField;
 
 	
 	/**
@@ -16,11 +29,64 @@ public class MainView extends JFrame {
 	 */
 	public MainView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 597, 714);
+		
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(61, 56, 70));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnDeleteProcess = new JButton("Eliminar Proceso");
+		btnDeleteProcess.setBounds(431, 266, 134, 27);
+		contentPane.add(btnDeleteProcess);
+		
+		JSlider sliderSpeed = new JSlider();
+		sliderSpeed.setBackground(new Color(61, 56, 70));
+		sliderSpeed.setForeground(new Color(61, 56, 70));
+		sliderSpeed.setValue(1);
+		sliderSpeed.setMaximum(10);
+		sliderSpeed.setMinimum(1);
+		sliderSpeed.setBounds(431, 348, 134, 16);
+		contentPane.add(sliderSpeed);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(431, 232, 134, 26);
+		contentPane.add(comboBox);
+		
+		Canvas canvas = new Canvas();
+		canvas.setForeground(new Color(94, 92, 100));
+		canvas.setBackground(new Color(119, 118, 123));
+		canvas.setBounds(30, 26, 287, 501);
+		contentPane.add(canvas);
+		
+		JButton btnCreateProcess = new JButton("Crear Proceso");
+		btnCreateProcess.setBounds(431, 193, 134, 27);
+		contentPane.add(btnCreateProcess);
+		
+		NameProcessField = new JTextField();
+		NameProcessField.setBounds(431, 103, 134, 21);
+		contentPane.add(NameProcessField);
+		NameProcessField.setColumns(10);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(526, 153, 39, 22);
+		contentPane.add(spinner);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setForeground(new Color(119, 118, 123));
+		lblNombre.setBounds(338, 105, 60, 17);
+		contentPane.add(lblNombre);
+		
+		JLabel lblTamao = new JLabel("Tamaño");
+		lblTamao.setForeground(new Color(119, 118, 123));
+		lblTamao.setBounds(338, 155, 60, 17);
+		contentPane.add(lblTamao);
+		
+		JLabel lblVelocidad = new JLabel("Velocidad");
+		lblVelocidad.setForeground(new Color(119, 118, 123));
+		lblVelocidad.setBounds(338, 347, 60, 17);
+		contentPane.add(lblVelocidad);
 	}
-
 }
